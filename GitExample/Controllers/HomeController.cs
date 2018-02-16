dae80d3abf9e8908fa95de923018002b72742b5e
risikyamaji <kyamajir@gmail.com>
2018-02-16 12:08:36 +0530

    .removed from the home controller

--- GitExample/Controllers/HomeController.cs
+++ GitExample/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace GitExample.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page";
 
             return View();
         }
