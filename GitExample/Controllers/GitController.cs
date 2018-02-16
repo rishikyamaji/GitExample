a1ba946d86f9ee379a88ae6194dba785e424b673
risikyamaji <kyamajir@gmail.com>
2018-02-16 12:35:20 +0530

    some code added to the git action

---
+++ GitExample/Controllers/GitController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitExample.Controllers
+{
+    public class GitController : Controller
+    {
+        // GET: Git
+        public ActionResult Index()
+        {
+            ViewBag.rishi = "this is rishi";
+            return View();
+        }
+    }
+}
